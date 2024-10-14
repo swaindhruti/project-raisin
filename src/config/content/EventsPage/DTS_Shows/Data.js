49c12f0352ca71738e4e089c73c305ad11dd439e
ayush <ayushatschool5@gmail.com>
2024-10-14 21:46:06 +0530

    chore: added events data

---
+++ src/config/content/EventsPage/DTS_Shows/Data.js
@@ -0,0 +1,38 @@
+const dtsShows = [
+  {
+    title: 'Showdown',
+    description:
+      'Prepare to be swept off your feet by Synergy! Be ready to be dazzled by their passion, energy, and creativity on stage. With a vibrant mix of dance styles, Showdown promises to be a thrilling display of talent and artistry. Join us for a night filled with captivating choreography, infectious rhythms, and powerful expressions as Synergy brings their unique stories to life through movement. This isn’t just a dance performance; it’s a celebration of unity, creativity, and the joy of expression.',
+    organizer: 'Synergy',
+    location: 'DTS Stage',
+    date: '',
+    time: '',
+  },
+  {
+    title: 'Trancenation',
+    description:
+      "Get ready to vibe with Drill N' Bass at Trancenation! Experience a night filled with exhilarating beats that blend EDM, pop, and hip-hop! Kick things off with a 45-minute live set of crowd favorites, followed by an epic 1-hour DJ set featuring original bangers from our crew.",
+    organizer: "Drill N' Bass",
+    location: 'DTS',
+    date: '',
+    time: '',
+  },
+  {
+    title: 'Euphony LIVE',
+    description:
+      'Euphony, the official rock band of NITR, having a legacy spanning over 25 years is back with another thrilling edition of Euphony LIVE! Get ready to rock the night away with an exhilarating set comprising of popular English and Hindi tracks. The conglomeration of Euphony’s dynamic presence, soulful vocals and rich instrumentals is sure to captivate the audience and evoke a sense of absolute euphoria. So grab your friends and join us in this electrifying atmosphere where music lovers unite!',
+    organizer: 'Euphony',
+    location: '',
+    date: '',
+    time: '',
+  },
+  {
+    title: 'Game Showcase',
+    description:
+      'Explore the enormous campus of NIT Rourkela virtually and play a VR game through Oculus Virtual Reality headsets. Embark on an immersive journey with Game Showcase, a virtual reality game and campus tour designed to transport you into a stunning digital landscape where exploration meets adventure. Experience your surroundings like never before. Get ready for an unforgettable exploration and gaming experience that combines fun, learning, and innovation!',
+    organizer: 'Game to Aim',
+    location: '',
+    date: '',
+    time: '',
+  },
+];
---
+++ src/config/content/EventsPage/Exhibition/Data.js
@@ -0,0 +1,29 @@
+const exibitions = [
+  {
+    title: 'IRIDESCENCE',
+    description:
+      'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity, showcasing an impressive array of artworks, including graphic illustrations, 3D models, typographic arts, crafts, fine arts, and more—all crafted by our talented club members. This year, IRIDESCENCE promises to be more dynamic, interactive, and visually captivating than ever, ensuring a memorable experience.',
+    organizer: 'Design Tab',
+    location: '',
+    date: '',
+    time: '',
+  },
+  {
+    title: 'RADIANCE 6.0',
+    description:
+      'We invite you to RADIANCE 6.0, an elaborate art and craft exhibition organised and hosted by Akriti, the official art and craft club of NITR. Every year, Radiance serves as a platform for artists to express their artistic visions and experiment with new techniques, bringing together a vibrant array of artworks of different art forms, including paintings, sculptures, handmade crafts and more. Join us for this inspiring experience and immerse yourself In the world of art!',
+    organizer: 'Akriti',
+    location: '',
+    date: '',
+    time: '',
+  },
+  {
+    title: 'SRIJAN 7.0',
+    description:
+      'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions and much more. These exhibits highlight architectural creativity, exploring various styles, forms, and techniques and delving into the connections and harmony between different architectural expressions and environments. Join us for SRIJAN 7.0 and experience euphoric!',
+    organizer: 'Society of Students of Planning and Architecture',
+    location: 'Rajendra Mishra (RM) Hall',
+    date: '',
+    time: '',
+  },
+];
